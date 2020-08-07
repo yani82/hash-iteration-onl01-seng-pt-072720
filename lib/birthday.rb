e2377eb269a3197f73803ddacc2738f0b8fe8ee9
Yani Asari <yani.iman1@gmail.com>
2020-08-07 07:22:06 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,7 +6,9 @@
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
-  "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
 end
 
 
