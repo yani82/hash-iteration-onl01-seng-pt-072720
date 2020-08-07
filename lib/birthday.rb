724a7c1aa1bad7f34ad7c3985c022d83d37221e1
Yani Asari <yani.iman1@gmail.com>
2020-08-07 07:31:32 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,8 +13,11 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    if age < 12
+  if age < 12
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    elsif 
+    puts "You are too old for this."
     end
+  end
   end
 end
